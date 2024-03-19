4b4a8d29152f20cec267cfc0d4ea6c7534b48a8e
tropswhy <solodkovnikita08@gmail.com>
2024-03-19 12:29:22 +0300

    add MovieStore.js

---
+++ src/stores/MovieStore.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+
+export const useMovieStore = defineStore ('MovieStore', {
+    state: () => ({
+        movies: [],
+    }),
+    
+    actions: {
+        
+    }
+})
