30aa75d2c6331c0c89ebe91eb42541dcad6a7cd2
tropswhy <solodkovnikita08@gmail.com>
2024-03-19 16:28:51 +0300

    add display MovieCards display

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -1,11 +1,26 @@
 <script setup>
+import { useMovieStore } from '../../stores/MovieStore.js'
+import MovieCard from '../ui/MovieCard.vue'
 
+const MovieStore = useMovieStore()
 </script>
 
 <template>
-
+    <v-app>
+        <v-container fluid>
+            <v-row dense>
+                <v-col
+                    class="text-center center"
+                    v-for="movie in MovieStore.movies.docs">
+                    <MovieCard
+                        :name="movie.name"
+                        :score="MovieStore.countAverageScore(movie)"
+                        :year="movie.year"
+                        :poster="movie.poster.previewUrl" />
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
 </template>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
