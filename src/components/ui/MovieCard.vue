c9a4d64329ee5fb7126e552c3312409f3a1dc2a3
tropswhy <solodkovnikita08@gmail.com>
2024-03-19 16:28:25 +0300

    add MovieCard component

---
+++ src/components/ui/MovieCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useMovieStore } from '../../stores/MovieStore.js'
+
+const movie = defineProps(['name', 'score', 'year', 'poster'])
+const MovieStore = useMovieStore()
+</script>
+
+<template>
+    <div class="d-flex align-center flex-column">
+        <v-card
+            class="my-2 align-end"
+            height="400px"
+            max-width="200px"
+            hover>
+            <v-img 
+            height="300px"
+            :src="poster"
+            cover/>
+            <v-card-title>
+                {{ name }}
+            </v-card-title>
+            <v-card-subtitle>
+                <p>{{ "Средняя оценка: " + score }}</p>
+                <p>{{ year + " года выпуска"}}</p>
+            </v-card-subtitle>
+        </v-card>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
