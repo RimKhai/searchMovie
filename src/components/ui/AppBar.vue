f307088e589e6b048a856e64a8436959256f449d
tropswhy <solodkovnikita08@gmail.com>
2024-03-29 17:15:29 +0300

    add AppBar component

---
+++ src/components/ui/AppBar.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    <v-toolbar
+        density="default"
+        scroll-behavior="hide">
+        <v-toolbar-title>ПоискКино</v-toolbar-title>
+        <v-spacer />
+        
+        <!-- <v-btn icon="mdi-magnify"/> -->
+    </v-toolbar>
+</template>
+
+<style lang="scss" scoped></style>
