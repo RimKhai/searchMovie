8f90e458d63b3a9a67cbfa5f8dcdae6874eee6cb
tropswhy <solodkovnikita08@gmail.com>
2024-03-29 17:15:59 +0300

    add search

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -1,17 +1,47 @@
 <script setup>
 import { useMovieStore } from '../../stores/MovieStore.js'
+import { ref, computed } from 'vue'
 import MovieCard from '../ui/MovieCard.vue'
+import AppBar from '../ui/AppBar.vue'
 
 const MovieStore = useMovieStore()
+
+const movies = ref(MovieStore.movies)
+
+const search = ref('')
+
+const searchMovies = computed(() => {
+    let m = movies.value.docs
+    let searchVal = search.value.toLowerCase()
+    if (searchVal){
+        m = m.filter((movie) => {
+            return movie.name.toLowerCase().indexOf(search.value.toLowerCase()) !== -1 
+        })
+    }
+    return m
+})
 </script>
 
 <template>
     <v-app>
-        <v-container fluid>
-            <v-row dense>
-                <v-col
-                    class="text-center center"
-                    v-for="movie in MovieStore.movies.docs">
+        <AppBar/>
+        <v-responsive class="mx-auto mt-8"
+        width="500">
+        <v-text-field
+            label="Поиск"
+            variant="outlined"
+            clearable
+            prepend-inner-icon="mdi-magnify"
+            single-line
+            v-model="search">
+        </v-text-field>
+    </v-responsive>
+        <v-container>
+            <v-row justify="start"
+            align="center">
+                <v-col 
+                class="text-center"
+                    v-for="movie in searchMovies">
                     <MovieCard
                         :name="movie.name"
                         :score="MovieStore.countAverageScore(movie)"
