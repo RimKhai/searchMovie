317eaea8e7958ccd0f3fe6bcaa1d6f8182435820
tropswhy <solodkovnikita08@gmail.com>
2024-04-09 18:17:34 +0300

    add sorting button

--- src/components/ui/AppBar.vue
+++ src/components/ui/AppBar.vue
@@ -1,13 +1,48 @@
-<script setup></script>
+<script setup>
+import { useMovieStore } from  '../../stores/MovieStore.js'
+const props = defineProps(['sortingParametrs'], 'currentSorting')
+const MovieStore = useMovieStore()
+
+const sortHandler = (param) => {
+    MovieStore.currentSorting = param;
+}
+
+const isActive = (currentItem) => {
+    if (currentItem === MovieStore.currentSorting) 
+        return 'green'
+    else
+        return false
+}
+</script>
 
 <template>
     <v-toolbar
         density="default"
         scroll-behavior="hide">
         <v-toolbar-title>ПоискКино</v-toolbar-title>
-        <v-spacer />
-        
-        <!-- <v-btn icon="mdi-magnify"/> -->
+        <!-- <v-spacer /> -->
+        <!-- <v-select
+        :items="sortingParametrs"
+        v-model="sorting"
+        label="Сортировка"
+        >
+        </v-select> -->
+        <v-btn>
+        Сортировка
+        <v-menu activator="parent">
+            <v-list>
+                <v-list-item
+                
+                color="green"
+                v-for="(item, index) in sortingParametrs"
+                :key="index"
+                :value="item.parametr"
+                @click="sortHandler(item.parametr)">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        </v-btn>
     </v-toolbar>
 </template>
 
