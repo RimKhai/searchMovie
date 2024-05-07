5edd5dfe567c36c5e5d1f7d5526e23959ae9ccc7
tropswhy <solodkovnikita08@gmail.com>
2024-05-07 15:33:44 +0300

    сделал начальный черновой вариант страницы фильма
    добавил условие, при котором кнопка сортировки повялется только на главной странице

--- src/components/ui/AppBar.vue
+++ src/components/ui/AppBar.vue
@@ -1,24 +1,23 @@
 <script setup>
-import { useMovieStore } from  '../../stores/MovieStore.js'
+import { useMovieStore } from '../../stores/MovieStore.js'
 const props = defineProps(['sortingParametrs'], 'currentSorting')
 const MovieStore = useMovieStore()
 
 const sortHandler = (param) => {
-    MovieStore.currentSorting = param;
+    MovieStore.currentSorting = param
 }
 
 const isActive = (currentItem) => {
-    if (currentItem === MovieStore.currentSorting) 
-        return 'green'
-    else
-        return false
+    if (currentItem === MovieStore.currentSorting) return 'green'
+    else return false
 }
 </script>
 
 <template>
     <v-toolbar
         density="default"
-        scroll-behavior="hide">
+        scroll-behavior="hide"
+    >
         <v-toolbar-title>ПоискКино</v-toolbar-title>
         <!-- <v-spacer /> -->
         <!-- <v-select
@@ -27,21 +26,21 @@ const isActive = (currentItem) => {
         label="Сортировка"
         >
         </v-select> -->
-        <v-btn>
-        Сортировка
-        <v-menu activator="parent">
-            <v-list>
-                <v-list-item
-                
-                color="green"
-                v-for="(item, index) in sortingParametrs"
-                :key="index"
-                :value="item.parametr"
-                @click="sortHandler(item.parametr)">
-                    <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-menu>
+        <v-btn v-if="$route.path === '/'">
+            Сортировка
+            <v-menu activator="parent">
+                <v-list>
+                    <v-list-item
+                        color="green"
+                        v-for="(item, index) in sortingParametrs"
+                        :key="index"
+                        :value="item.parametr"
+                        @click="sortHandler(item.parametr)"
+                    >
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </v-btn>
     </v-toolbar>
 </template>
