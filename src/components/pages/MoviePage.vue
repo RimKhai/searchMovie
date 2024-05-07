0528e4abe91b7897731a7e480b4d9d9c2e7a2fbb
tropswhy <solodkovnikita08@gmail.com>
2024-05-07 15:33:44 +0300

    сделал начальный черновой вариант страницы фильма

--- src/components/pages/MoviePage.vue
+++ src/components/pages/MoviePage.vue
@@ -1,12 +1,24 @@
 <script setup>
 import { useMovieStore } from '../../stores/MovieStore.js'
+import { computed, ref } from 'vue'
+import { useRoute } from 'vue-router'
 import AppBar from '../ui/AppBar.vue'
+const movieStore = useMovieStore()
+const route = useRoute()
+const movieId = computed(() => route.params.movieId)
+const movie =  movieStore.findMovieById(movieId.value)
+/* console.log(movieStore.findMovieById(movieId)) */
 </script>
 
 <template>
-
+    <AppBar />
+    <h1>
+        {{ movie.name }}
+    </h1>
+    <h2 v-if="movie.alternativeName">
+        Оригинальное название:
+        {{ movie.alternativeName }}
+    </h2>
 </template>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
