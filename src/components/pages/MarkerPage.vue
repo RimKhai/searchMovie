7e4614759a41a907afd9bad91a77bd7e1f43881a
tropswhy <solodkovnikita08@gmail.com>
2024-07-02 15:23:18 +0300

    черновой вариант страницы с закладками

--- src/components/pages/MarkerPage.vue
+++ src/components/pages/MarkerPage.vue
@@ -1,11 +1,59 @@
 <script setup>
-
+import { useMovieStore } from '../../stores/MovieStore.js'
+import { ref, computed } from 'vue'
+import { useRoute } from 'vue-router'
+import MovieCard from '../ui/MovieCard.vue'
+import AppBar from '../ui/AppBar.vue'
+window.scrollTo(0, 0)
+const movieStore = useMovieStore()
+const movieLS = ref({})
+const movies = movieStore.movies.docs
+const markedMovies = computed(() =>
+    movies.filter((movie) => {
+        movieLS.value = JSON.parse(localStorage.getItem(movie.name))
+        if (movieLS.value && movieLS.value.isMark) {
+            console.log(movie.name)
+            console.log(movieLS.value)
+            return true
+        } else {
+            return false
+        }
+    })
+)
+function unmark (name) {
+    const movieLS = JSON.parse(localStorage.getItem(name))
+    movieLS.isMark = false
+    localStorage.setItem(name, JSON.stringify(movieLS))
+    //markedMovies
+}
+/* const unmark = computed(() => {
+    const movieLS = JSON.parse(localStorage.getItem(movie.name.toString()))
+    movieLS.isMark = false
+    localStorage.setItem(movie.name.toString(), JSON.stringify(movieLS))
+    markedMovies
+}) */
 </script>
 
 <template>
-
+    <AppBar />
+    <v-container>
+        <v-row justify="start">
+            <v-col
+                class="text-center"
+                v-for="movie in markedMovies"
+                :key="movie.name"
+            >
+                <MovieCard
+                    :name="movie.name"
+                    :score="movieStore.countAverageScore(movie)"
+                    :year="movie.year"
+                    :poster="movie.poster.previewUrl"
+                    :id="movie.externalId._id"
+                    @unmark-movie="unmark(movie.name)"
+                />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
